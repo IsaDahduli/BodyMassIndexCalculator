0cd2e2402729779cd7306d586bd8c91761f697d5
Isa Dahduli <isa95dev@hotmail.com>
2024-06-05 12:41:52 +0300

    Fix

--- app/src/main/java/com/isa/bodymassindexcalculator/MainActivity.kt
+++ app/src/main/java/com/isa/bodymassindexcalculator/MainActivity.kt
@@ -6,12 +6,14 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
         val calculateButton: Button = findViewById(R.id.btn_calculate)
         val resetButton: Button = findViewById(R.id.btn_reset)
